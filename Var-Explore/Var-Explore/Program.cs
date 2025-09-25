e0940b043c9cf120ecd774dcea7a25cf36fa6002
AdamG6754 <adam.goltsman@gmail.com>
2025-09-25 08:36:48 -0400

    init

---
+++ Var-Explore/Var-Explore/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Var_Explore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            string heroFirstName = "Adam";
+            string heroLastName = "Goltsman";
+            string heroFullName;
+
+            //begin code:
+
+        }
+    }
+}
