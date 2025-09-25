9023834937dedb34acb913ac1f30f1b7513ef221
AdamG6754 <adam.goltsman@gmail.com>
2025-09-25 09:02:39 -0400

    added villain and minions

--- Var-Explore/Var-Explore/Program.cs
+++ Var-Explore/Var-Explore/Program.cs
@@ -8,9 +8,17 @@
             string heroFirstName = "Adam";
             string heroLastName = "Goltsman";
             string heroFullName;
+            string villainTitle = "Doom and Peril";
+            string minionTitle = "Minimum Wage Underling";
+            string villainName;
+            string minion1Name;
+            string minion2Name;
 
             //begin code:
 
+            heroFullName = heroFirstName + " " + heroLastName;
+
+            Console.WriteLine("Here comes the hero! Put your hands together for " + heroFullName);
         }
     }
 }
